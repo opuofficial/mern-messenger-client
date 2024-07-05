9e82cb70fa7faca0b4b3a1501c129c8f88084a8d
Opu <opuofficial24@gmail.com>
2024-07-05 18:48:59 +0600

    redirect root route to main page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import AuthLayout from "../layouts/AuthLayout";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
@@ -8,6 +8,10 @@ import Conversation from "../components/Conversation";
 import ProtectedRoute from "./ProtectedRoute";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    loader: () => redirect("/messages"),
+  },
   {
     path: "/auth",
     element: <AuthLayout />,
